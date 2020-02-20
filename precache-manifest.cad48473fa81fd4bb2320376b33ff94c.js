fc32673d8481500015f2551ca81684be64d0ab2f
Lerka Dolzhenko <lerka1999dolzhenko@gmail.com>
2020-02-20 16:45:04 +0200

    Updates

---
+++ precache-manifest.cad48473fa81fd4bb2320376b33ff94c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "952ca1e55437a4a1aa82f946809e6afd",
+    "url": "/bizicoPractice/index.html"
+  },
+  {
+    "revision": "70e6f439a9c0dc2c7a26",
+    "url": "/bizicoPractice/static/css/main.86473d3e.chunk.css"
+  },
+  {
+    "revision": "37234697302e4671f591",
+    "url": "/bizicoPractice/static/js/2.b49b2f5b.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/bizicoPractice/static/js/2.b49b2f5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70e6f439a9c0dc2c7a26",
+    "url": "/bizicoPractice/static/js/main.801c480e.chunk.js"
+  },
+  {
+    "revision": "44d7da9c6fc04d914d5e",
+    "url": "/bizicoPractice/static/js/runtime-main.3540215c.js"
+  }
+]);
